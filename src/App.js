a0e804cb57f7eb1c054c9f72a0a6e8fb6ef7f6fa
ifebrand6 <ifebrand6@gmail.com>
2021-01-17 03:03:37 +0100

    setup application route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import React from 'react'
 import './assets/stylesheet/authentication.css';
 import Authentication from './components/authentication/authentication';
-
+import { BrowserRouter,Route, Switch } from 'react-router-dom'
 import Footer from './components/shared/footer';
 import LandingPage from './main/landingPage';
 
 function App() {
   return (
     <>
-      {/* <LandingPage></LandingPage> */}
-      <Authentication></Authentication>
-      {/* <Footer></Footer> */}
+    <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <LandingPage/>
+        <Footer/>
+      </Route>
+      <Route path="/login">
+        <Authentication/>
+      </Route>
+    </Switch>
+    </BrowserRouter>
     </>
   );
 }
